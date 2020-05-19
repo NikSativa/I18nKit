a9406339a36bb889236517a2f3b92a3b329721b5
Nikita Konopelko <nik.sativa@gmail.com>
2020-05-19 11:39:17 +0300

    small helper

--- Source/Test/FakeI18n.swift
+++ Source/Test/FakeI18n.swift
@@ -24,12 +24,23 @@ class FakeI18n<Key: I18nKey>: I18n<Key>, Spryable where Key: SpryEquatable {
         return spryify(arguments: key)
     }
 
+    // helper to make stubbing like
+    // `i18n.stub(.tWith).with(key, "one parameter").andReturn("result")`
+    // instead of
+    // `i18n.stub(.tWith).with(key, ["one parameter"]).andReturn("result")`
+    private func prepare(_ parameters: [CVarArg]) -> Any {
+        if parameters.count == 1 {
+            return parameters[0]
+        }
+        return parameters
+    }
+
     override func t(_ key: Key, with parameters: CVarArg...) -> String {
-        return spryify(arguments: key, parameters)
+        return spryify(arguments: key, prepare(parameters))
     }
 
     override func t(_ key: Key, with parameters: [CVarArg]) -> String {
-        return spryify(arguments: key, parameters)
+        return spryify(arguments: key, prepare(parameters))
     }
 
     override func v(_ key: Key) -> I18nValue {
@@ -37,10 +48,10 @@ class FakeI18n<Key: I18nKey>: I18n<Key>, Spryable where Key: SpryEquatable {
     }
 
     override func v(_ key: Key, with parameters: CVarArg...) -> I18nValue {
-        return spryify(arguments: key, parameters)
+        return spryify(arguments: key, prepare(parameters))
     }
 
     override func v(_ key: Key, with parameters: [CVarArg]) -> I18nValue {
-        return spryify(arguments: key, parameters)
+        return spryify(arguments: key, prepare(parameters))
     }
 }
