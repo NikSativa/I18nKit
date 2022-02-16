017844c55f983377103217fae1113de325bd2528
Nikita Konopelko <nik.sativa@gmail.com>
2022-02-16 12:25:09 +0200

    new parameters of SwiftFormat

--- Tests/Bundle+Module.swift
+++ Tests/Bundle+Module.swift
@@ -5,8 +5,6 @@ private final class BundleFinder {
 }
 
 extension Foundation.Bundle {
-    static let module: Bundle = {
-        Bundle(for: BundleFinder.self)
-    }()
+    static let module: Bundle = .init(for: BundleFinder.self)
 }
 #endif
